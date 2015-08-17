caf9f7e84963682a77a11ff37f7047204eb4c4fc
hanxirui <han_xirui@163.com>
2015-08-17 13:32:19 +0800

    chapter3 recipe4
    chapter3 recipe4

---
+++ javastone/src/main/java/com/hxr/javatone/concurrency/java7concurrency/chapter3/recipe4/Grouper.java
@@ -0,0 +1,39 @@
+package com.hxr.javatone.concurrency.java7concurrency.chapter3.recipe4;
+
+
+
+/**
+ * Group the results of each Searcher. Sum the values stored in the Results object 
+ * An object of this class is executed automatically by the CyclicBarrier when
+ * all the Searchers finish its job
+ */
+public class Grouper implements Runnable {
+
+	/**
+	 * Results object with the occurrences of the number in each row
+	 */
+	private final Results results;
+	
+	/**
+	 * Constructor of the class. Initializes its attributes
+	 * @param results Results object with the ocurrences of the number in each row
+	 */
+	public Grouper(final Results results){
+		this.results=results;
+	}
+	
+	/**
+	 * Main method of the Grouper. Sum the values stored in the Results object 
+	 */
+	@Override
+	public void run() {
+		int finalResult=0;
+		System.out.printf("Grouper: Processing results...\n");
+		int data[]=results.getData();
+		for (int number:data){
+			finalResult+=number;
+		}
+		System.out.printf("Grouper: Total result: %d.\n",finalResult);
+	}
+
+}
