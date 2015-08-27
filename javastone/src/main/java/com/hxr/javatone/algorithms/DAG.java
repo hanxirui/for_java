ee6c9ecb6656912c624070601fe340e0825d11b2
hanxirui <han_xirui@163.com>
2015-08-28 07:39:17 +0800

    Floyd
    Floyd

---
+++ javastone/src/main/java/com/hxr/javatone/algorithms/DAG.java
@@ -0,0 +1,24 @@
+package com.hxr.javatone.algorithms;
+
+/**
+ * {一个无环的有向图称为有向无环图，简写为DAG（directed acycline graph）}
+ * 应用：拓扑排序（选课）
+ * <br>
+ *  
+ * <p>
+ * Create on : 2015年8月22日<br>
+ * <p>
+ * </p>
+ * <br>
+ * @author hanxirui<br>
+ * @version javastone v1.0
+ * <p>
+ *<br>
+ * <strong>Modify History:</strong><br>
+ * user     modify_date    modify_content<br>
+ * -------------------------------------------<br>
+ * <br>
+ */
+public class DAG {
+
+}
---
+++ javastone/src/main/java/com/hxr/javatone/algorithms/RedBlackTree.java
@@ -0,0 +1,30 @@
+package com.hxr.javatone.algorithms;
+
+/**
+ *1.每个结点要么是红色要么是黑色；
+
+2.根结点必须是黑色；
+
+3.每个叶节点,即空节点(NULL)是黑色的。
+
+4.红结点如果有孩子，其孩子必须都是黑色(红节点周边3个节点都是黑色)；
+
+5.从根结点到叶子的每条路径必须包含相同数目的黑结点。
+ *  
+ * <p>
+ * Create on : 2015年8月27日<br>
+ * <p>
+ * </p>
+ * <br>
+ * @author hanxirui<br>
+ * @version javastone v1.0
+ * <p>
+ *<br>
+ * <strong>Modify History:</strong><br>
+ * user     modify_date    modify_content<br>
+ * -------------------------------------------<br>
+ * <br>
+ */
+public class RedBlackTree {
+
+}
